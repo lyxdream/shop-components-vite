4418c2758247505c2a06b31cb8539c2697cacd32
liyinxia <liyinxia@weimiao.cn>
2024-11-21 18:42:54 +0800

    feat: 修改文件

--- build/vite.config.build.resolver.ts
+++ build/vite.config.build.resolver.ts
@@ -20,6 +20,8 @@ export default defineConfig({
       formats: ['es', 'cjs']
     },
     rollupOptions: {
+      // 请确保外部化那些你的库中不需要的依赖
+      external: ['vue', 'vue-router', '@tarojs/taro'],
       output: {
         dir: path.resolve(__dirname, '../dist/resolver')
       }
